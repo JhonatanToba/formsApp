69550d7aee26dad83a856fbeda127553a1a387d7
Jhonatan Leonardo Toba Silva <leonardotoba1998@gmail.com>
2024-04-23 12:42:22 -0500

    Se realiza la conexión y se empieza a ver funcionamiento de Arrays en formularios

--- src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
+++ src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './dynamic-page.component.html',
@@ -6,4 +7,32 @@ import { Component } from '@angular/core';
 })
 export class DynamicPageComponent {
 
+  public dynamicForm: FormGroup = this.fb.group({
+    name: ['', [Validators.required, Validators.minLength(3)] ],
+    favoriteGames: this.fb.array([
+      ['Metal Gear', Validators.required],
+      ['Death Stranding', Validators.required],
+    ]),
+
+  })
+
+  constructor( private fb: FormBuilder){}
+
+
+  public get favoriteGames() {
+    return this.dynamicForm.get('favoriteGames') as FormArray;
+  }
+
+
+  onSubmit(): void {
+
+    if (this.dynamicForm.invalid) {
+      this.dynamicForm.markAllAsTouched();
+      return;
+    }
+
+    console.log(this.dynamicForm.value);
+
+    this.dynamicForm.reset();
+  }
 }
