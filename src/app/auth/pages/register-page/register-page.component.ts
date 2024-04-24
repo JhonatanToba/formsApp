e2c703395c265499af4d27ab91eca986dd3768ff
Jhonatan Leonardo Toba Silva <leonardotoba1998@gmail.com>
2024-04-24 11:50:07 -0500

    Se añade función de validación propia para el Username

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+import { cantBeStrider } from '../../../shared/validators/validators';
 
 @Component({
   templateUrl: './register-page.component.html',
@@ -6,4 +9,21 @@ import { Component } from '@angular/core';
 })
 export class RegisterPageComponent {
 
+  public myForm: FormGroup = this.fb.group({
+    name: ['', [ Validators.required ] ],
+    email: ['', [ Validators.required, Validators.email ] ],
+    username: ['', [ Validators.required, cantBeStrider ] ],
+    password: ['', [ Validators.required, Validators.minLength(6) ] ],
+    password2: ['', [ Validators.required ] ],
+  })
+
+  constructor ( private fb: FormBuilder ){}
+
+  isValidField( field: string){
+    //TODO: Obtenerlo desde un servicio.
+  }
+
+  onSubmit(){
+    this.myForm.markAllAsTouched()
+  }
 }
